6dccacb445e7b9e6549a931b8efd5e51a91b5511
Fagner Santos <uprogramador@gmail.com>
2022-07-23 16:50:11 -0300

    Primeiro commit

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
