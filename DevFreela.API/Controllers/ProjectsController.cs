c9962d09f76ba5bc25aaf06f1c056b25f1f31973
Fagner Santos <uprogramador@gmail.com>
2022-07-23 17:24:44 -0300

    Foram criados as Models e os verbos http

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace DevFreela.API.Controllers
@@ -6,10 +7,62 @@ namespace DevFreela.API.Controllers
     [Route("api/projects")]
     public class ProjectsController : ControllerBase
     {
+        // api/projects?query=net core
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
+            // Buscar todos ou filtrar
+
             return Ok();
+        }
+
+        // api/projects/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // Buscar o projeto
+
+            // return NotFound
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if(createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            // Cadastrar o projeto
+
+            return CreatedAtAction(nameof(GetById), new {id = createProject.Id}, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            // Atualizo o objeto
+
+            return NoContent();
+        }
+
+        // api/projects/3
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retorna NotFound
+
+            // Remover
+
+            return NoContent();
         }
     }
 }
